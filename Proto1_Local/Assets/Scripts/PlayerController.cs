81d4f287e7293746eec64e5b0b0233007edd8b89
adrinuga <38584756+adrinuga@users.noreply.github.com>
2018-12-25 05:08:01 +0100

    Improves

--- Proto1_Local/Assets/Scripts/PlayerController.cs
+++ Proto1_Local/Assets/Scripts/PlayerController.cs
@@ -40,7 +40,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         m_actualSpeed = m_defaultSpeed;
-        
+        m_GameCamera = Camera.main;
     }
 
     // Update is called once per frame
@@ -48,6 +48,7 @@ public class PlayerController : MonoBehaviour
     {
         UpdateMovement();
         UpdateMouse();
+        CameraUpdate();
 
         Flip();
 
@@ -88,8 +89,8 @@ public class PlayerController : MonoBehaviour
     void UpdateMouse()
     {
         Vector3 l_MousePos = Input.mousePosition;
-        l_MousePos.z = -Camera.main.transform.position.z;
-        Vector3 l_objectPos = Camera.main.WorldToScreenPoint(m_WeaponTransform.position);
+        l_MousePos.z = -m_GameCamera.transform.position.z;
+        Vector3 l_objectPos = m_GameCamera.WorldToScreenPoint(m_WeaponTransform.position);
 
         l_MousePos.x -= l_objectPos.x;
         l_MousePos.y -= l_objectPos.y;
@@ -101,22 +102,28 @@ public class PlayerController : MonoBehaviour
     }
     void Flip()
     {
-        float l_MouseX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
-        float l_MouseY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+        float l_MouseX = m_GameCamera.ScreenToWorldPoint(Input.mousePosition).x;
+        float l_MouseY = m_GameCamera.ScreenToWorldPoint(Input.mousePosition).y;
 
         float l_WeaponZ = Mathf.Abs( m_WeaponTransform.position.z);
+        //float l_WeaponX = Mathf.Abs(m_WeaponTransform.position.x);
 
-        //Vector3 l_scale = transform.localScale;
+        Vector3 l_scale = transform.localScale;
 
         if (l_MouseX > transform.position.x)
         {
             m_WeaponRenderer.flipY = false;
-            m_myRenderer.flipX = true;
+            m_WeaponRenderer.flipX = true;
+            //m_myRenderer.flipX = false;
+            l_scale.x = 1;
         }
         else
         {
             m_WeaponRenderer.flipY = true;
-            m_myRenderer.flipX = false;
+            m_WeaponRenderer.flipX = false;
+            // m_myRenderer.flipX = true;
+            // l_WeaponX *= -1;
+            l_scale.x = -1;
         }
             
         if(l_MouseY < transform.position.y)
@@ -125,9 +132,13 @@ public class PlayerController : MonoBehaviour
         }
        
         m_WeaponTransform.position = new Vector3(m_WeaponTransform.position.x, m_WeaponTransform.position.y, l_WeaponZ);
-        //transform.localScale = l_scale;
+        transform.localScale = l_scale;
 
 
+
+    }
+    void CameraUpdate()
+    {
 
     }
     public void ManageAbility()
